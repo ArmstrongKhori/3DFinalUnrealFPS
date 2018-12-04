41c040c03895cace1e5bdaeca6299e1e7e29eedd
Khori Armstrong <39284393+ArmstrongKhori@users.noreply.github.com>
2018-12-03 20:39:03 -0500

    system objects
    also some inputters

---
+++ Assets/Scripts/Inputter/BaseInputter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseInputter {
+
+    public float moving = 0.0f;
+    public float strafing = 0.0f;
+    public bool jump = false;
+    public bool fire1 = false;
+    public bool fire2 = false;
+
+    
+    public virtual void Read()
+    {
+        moving = 0.0f;
+        strafing = 0.0f;
+        jump = false;
+        fire1 = false;
+        fire2 = false;
+    }
+}
---
+++ Assets/Scripts/SystemObjs/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SystemObj {
+
+
+
+
+
+    private static GameManager _instance;
+    public static GameManager Instance() { return _instance; }
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        //
+        DontDestroyOnLoad(this.gameObject);
+
+
+        Initialize();
+    }
+}
