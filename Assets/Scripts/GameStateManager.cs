706138e3388ec03c09a13022837d096ab65e2b55
Khori Armstrong <39284393+ArmstrongKhori@users.noreply.github.com>
2018-12-04 20:08:05 -0500

    improved statemanager

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -16,17 +16,58 @@ public class GameStateManager : MonoBehaviour {
 
         cl_GameStateManager = this;
     }
-    #endregion 
+    #endregion
 
     public GameObject[] gameStates;
-    public enum GameStates { MENU, GAME, PAUSE, GAMEOVER, GAMEWIN }
-    private GameStates e_gameStates;
-   
+    public GameObject[] gameplayGameStates;
 
-    public bool b_IsGamePaused = false;
-    public bool b_IsGameModeActive = false;
-    public bool b_IsGameOver = false;
-    public bool b_IsGameWin = false;
+    public enum MajorGameStates { MENU, GAME, GAMEOVER, GAMEWIN }
+    private MajorGameStates e_majorGameStates;
+    public void DoChangeMajorGameState(MajorGameStates state)
+    {
+        gameStates[(int)e_majorGameStates].SetActive(false);
+        //
+        //
+        e_majorGameStates = state;
+        //
+        //
+        gameStates[(int)e_majorGameStates].SetActive(true);
+        switch (e_majorGameStates)
+        {
+            case MajorGameStates.GAME:
+                DoChangeGameplayGameState(GameplayGameStates.RUNNING);
+                break;
+        }
+    }
+
+
+    public enum GameplayGameStates { RUNNING, PAUSED }
+    private GameplayGameStates e_gameplayGameStates;
+    public void DoChangeGameplayGameState(GameplayGameStates state)
+    {
+        gameplayGameStates[(int)e_gameplayGameStates].SetActive(false);
+        //
+        //
+        e_gameplayGameStates = state;
+        //
+        //
+        gameplayGameStates[(int)e_gameplayGameStates].SetActive(true);
+        switch (e_gameplayGameStates)
+        {
+            case GameplayGameStates.PAUSED:
+                Time.timeScale = 0f;
+                break;
+            case GameplayGameStates.RUNNING:
+                Time.timeScale = 1f;
+                break;
+        }
+
+    }
+
+    public bool IsGameActive { get { return (e_gameplayGameStates == GameplayGameStates.RUNNING); } }
+    public bool IsGameOver { get { return (e_majorGameStates == MajorGameStates.GAMEOVER || e_majorGameStates == MajorGameStates.GAMEWIN); } }
+    public bool IsGameWon { get { return (e_majorGameStates == MajorGameStates.GAMEWIN); } }
+    public bool IsGamePaused { get { return (e_majorGameStates == MajorGameStates.GAME) && !IsGameActive; } }
 
 
     private void Start()
@@ -38,57 +79,33 @@ public class GameStateManager : MonoBehaviour {
     // Game Manu State
     public void GameMenu()
     {
-        b_IsGameModeActive = false;
-        b_IsGamePaused = false;
-        b_IsGameOver = false;
-        b_IsGameWin = false;
-        e_gameStates = GameStates.MENU;
-        ChangeGameStates();
-
+        DoChangeMajorGameState(MajorGameStates.MENU);
     }
 
     // Game State
     public void StartGame()
     {
-        b_IsGameModeActive = true;
-        e_gameStates = GameStates.GAME;
-        ChangeGameStates();
-
+        DoChangeMajorGameState(MajorGameStates.GAME);
     }
     // Activate the Pause State
     public void PauseGame()
     {
-        e_gameStates = GameStates.PAUSE;
-        ChangeGameStates();
-        b_IsGameModeActive = false;
-        b_IsGamePaused = true;
-        Time.timeScale = 0f;
-
+        DoChangeGameplayGameState(GameplayGameStates.PAUSED);
     }
     //Unpause game
     public void UnpausedGame()
     {
-        e_gameStates = GameStates.GAME;
-        ChangeGameStates();
-        b_IsGameModeActive = true;
-        b_IsGamePaused = false;
-        Time.timeScale = 1f;
+        DoChangeGameplayGameState(GameplayGameStates.RUNNING);
     }
     // Game Over State
     internal void GameOver()
     {
-        gameStates[1].SetActive(false);
-        gameStates[3].SetActive(true);
-        b_IsGameOver = true;
-
+        DoChangeMajorGameState(MajorGameStates.GAMEOVER);
     }
     // Game Win State
     internal void GameWin()
     {
-        gameStates[1].SetActive(false);
-        gameStates[4].SetActive(true);
-        b_IsGameWin = true;
-
+        DoChangeMajorGameState(MajorGameStates.GAMEWIN);
     }
     // Game Exit
     public void GameExit()
@@ -99,63 +116,8 @@ public class GameStateManager : MonoBehaviour {
 
     public void ReloadScene()
     { SceneManager.LoadScene(0); }
+    
 
-
-
-    // Switching between states based on ENUM
-    private void ChangeGameStates()
-    {
-
-        switch (e_gameStates)
-        {
-            case GameStates.MENU:
-                gameStates[0].SetActive(true); // MENU
-                gameStates[1].SetActive(false); // GAME
-                gameStates[2].SetActive(false); // PAUSE
-                gameStates[3].SetActive(false); // GAMEOVER
-                gameStates[4].SetActive(false); // GAMEWIN
-
-
-                break;
-
-            case GameStates.GAME:
-                gameStates[0].SetActive(false); // MENU
-                gameStates[1].SetActive(true); // GAME
-                gameStates[2].SetActive(false); // PAUSE
-                gameStates[3].SetActive(false); // GAMEOVER
-                gameStates[4].SetActive(false); // GAMEWIN
-
-                break;
-
-            case GameStates.PAUSE:
-                gameStates[0].SetActive(false); // MENU
-                gameStates[1].SetActive(true); // GAME
-                gameStates[2].SetActive(true); // PAUSE
-                gameStates[3].SetActive(false); // GAMEOVER
-                gameStates[4].SetActive(false); // GAMEWIN
-                break;
-
-            case GameStates.GAMEOVER:
-                gameStates[0].SetActive(false); // MENU
-                gameStates[1].SetActive(false); // GAME
-                gameStates[2].SetActive(false); // PAUSE
-                gameStates[3].SetActive(true); // GAMEOVER
-                gameStates[4].SetActive(false); // GAMEWIN
-
-                break;
-
-            case GameStates.GAMEWIN:
-                gameStates[0].SetActive(false); // MENU
-                gameStates[1].SetActive(false); // GAME
-                gameStates[2].SetActive(false); // PAUSE
-                gameStates[3].SetActive(false); // GAMEOVER
-                gameStates[4].SetActive(true); // GAMEWIN
-
-                break;
-
-        }
-
-    }
 
 
     void Update () {
@@ -165,30 +127,24 @@ public class GameStateManager : MonoBehaviour {
         {
             GameMenu();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2) && !b_IsGameOver)
+        if (Input.GetKeyDown(KeyCode.Alpha2) && !IsGameOver)
         {
             StartGame();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3) && !b_IsGameOver)
+        if (Input.GetKeyDown(KeyCode.Alpha3) && !IsGameOver)
         {
-            if (b_IsGamePaused)
+            if (IsGamePaused)
             {
                 UnpausedGame();
             }
             else { PauseGame();}
         }
-        if (Input.GetKeyDown(KeyCode.Alpha4) && !b_IsGameOver && b_IsGameModeActive)
+        if (Input.GetKeyDown(KeyCode.Alpha4) && !IsGameOver)
         {
             GameOver();
         }
-        if (Input.GetKeyDown(KeyCode.Alpha5) && !b_IsGameWin && b_IsGameModeActive)
+        if (Input.GetKeyDown(KeyCode.Alpha5) && !IsGameOver)
         {
-            gameStates[4].SetActive(false);
-            GameOver();
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha6) && !b_IsGameOver && b_IsGameModeActive)
-        {
-            gameStates[3].SetActive(false);
             GameWin();
         }
     }
