0950601cbaeb37dcff514e27d425600f849e6e27
Mike <mikeemorra@gmail.com>
2018-12-12 20:21:24 -0500

    adde bounce pad

---
+++ Assets/MIKE/Assets/BouncePadControl.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePadControl : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnCollisionEnter(Collision collision)
+    {
+     if (collision.gameObject.tag == "Player")
+        {
+            // BOUNCE PLAYER 2X HEIGHT
+            //collision.gameObject.GetComponent<>
+        }   
+    }
+}
