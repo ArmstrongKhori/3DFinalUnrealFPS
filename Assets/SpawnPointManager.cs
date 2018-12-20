16ca6bb97d96c158dc72d46c6216bac537ebbe29
Mike <mikeemorra@gmail.com>
2018-12-19 19:22:39 -0500

    Spawning
    Spawn points and spawn manager

---
+++ Assets/SpawnPointManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnPointManager : MonoBehaviour {
+    public NetworkManager CurrentNetManager;
+    public GameObject[] BlueTeamSpawnPointList;
+    public GameObject[] RedTeamSpawnPointList;
+	void Start () {
+        CurrentNetManager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+        BlueTeamSpawnPointList = GameObject.FindGameObjectsWithTag("BlueTeamSpawnPoint");
+        RedTeamSpawnPointList = GameObject.FindGameObjectsWithTag("RedTeamSpawnPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Decide team and spawn point for connected player 
+	}
+}
